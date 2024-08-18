a93865ee4bd73ac0515a75039c70173aeb7e604e
Sakshamkhanna132 <sakshamkhanna132@gmail.com>
2024-08-18 15:21:33 +0530

    Completed code for adding two integers.

--- Sum.c
+++ Sum.c
@@ -1,2 +1,19 @@
 /* In this file we will add 2 integers. */
 
+#include <stdio.h>
+int main(){
+
+int num1;
+int num2;
+
+printf("Enter the first number");
+scanf("%d", &num1);
+
+printf("Enter the sencond number");
+scanf("%d", &num2);
+
+printf("Sum = %d", num1 + num2);
+
+
+return 0;
+}
